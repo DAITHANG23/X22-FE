1dfd41d6c629d2103a70963fcf9fefa4552a0bb3
trananh-it-hust <haianhtranmanh@gmail.com>
2024-04-14 16:25:18 +0700

    ha14/04

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -39,7 +39,7 @@ const Login = () => {
       })
       .catch((error) => {
         console.log(error);
-        setErrorMessage("Invalid email or password");
+        setErrorMessage("Sai gmail hoặc mật khẩu");
       })
       .finally(() => {
         setIsLoading(false);
@@ -58,7 +58,7 @@ const Login = () => {
     <div className="login-container">
       {!isLogin && (
         <div>
-          <h2>Login</h2>
+          <h2>Đăng nhập</h2>
           <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="email">Email:</label>
@@ -72,7 +72,7 @@ const Login = () => {
               />
             </div>
             <div className="form-group">
-              <label htmlFor="password">Password:</label>
+              <label htmlFor="password">Mật khẩu:</label>
               <input
                 type="password"
                 id="password"
@@ -87,14 +87,14 @@ const Login = () => {
             <div className="form-group">
               <input
                 type="submit"
-                value={isLoading ? "Logging in..." : "Login"}
+                value={isLoading ? "Đang đăng nhập..." : "Đăng nhập"}
                 disabled={isLoading}
               />
             </div>
           </form>
           <div>
             <p>
-              Don't have an account? <Link to="/register">Sign Up</Link>
+              Bạn chưa có tài khoản? <Link to="/register">Đăng ký</Link>
             </p>
           </div>
         </div>
